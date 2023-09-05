34b131c11c1e69b4a9805e5ab374d404061659bc
cakiya <cakiya@cakiyas-mbp.wireless.rit.edu>
2023-09-05 18:27:44 -0400

    initial commit of hirakana tester

---
+++ cakiya's_hirakana_tester.py
@@ -0,0 +1,48 @@
+"""
+This program picks a random hirakana romaji and displays
+it to the user (so they can write down the corresponding
+hiragana and katakana as practice) and keeps on going until
+every hirakana romaji has been tested; then it crashes becasue
+i dont know im too lazy to implement not crashing and re-usability
+"""
+
+
+import random
+
+GLOBALVAR_HIRAKANA_ROMAJI = [
+    "a", "i", "u", "e", "o",
+    "ka", "ki", "ku", "ke", "ko",
+    "sa", "shi", "su", "se", "so",
+    "ta", "chi", "tsu", "te", "to",
+    "na", "ni", "nu", "ne", "no",
+    "ha", "hi", "fu", "he", "ho",
+    "ma", "mi", "mu", "me", "mo",
+    "ya", "yu", "yo",
+    "ra", "ri", "ru", "re", "ro",
+    "wa", "wo", "n"
+]
+
+test_list = GLOBALVAR_HIRAKANA_ROMAJI.copy()
+
+print(GLOBALVAR_HIRAKANA_ROMAJI)
+
+def test_loop():
+    hirakana_count = len(GLOBALVAR_HIRAKANA_ROMAJI)
+    for i in range(hirakana_count):
+        print(test_one_hirakana())
+        input("Press enter to Proceed")
+
+def test_one_hirakana():
+    """
+    chooses and removes a random hirakana from
+    the hirakana list
+    """
+    random_hirakana_in_list = random.randint(0, len(test_list))
+    return test_list.pop(random_hirakana_in_list)
+
+def main():
+    print("BEGINNING TEST")
+    test_loop()
+
+# Don't forget to call main()!
+main()
